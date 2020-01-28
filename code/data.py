fix unmatch length in loss calculation, first test training

code/data.py
@@ -21,7 +21,7 @@ Raw audio and features dataset definition
 """
 class AudioFeaturesDataset(Dataset):
     """
-    Create a dataseet of mixed audio, fft and features. Each of the original 
+    Create a dataseet of mixed audio, fft and features. Each of the original
     audio tracks will be processed by first slicing it into blocks X sequence
     elements. Each element is the used as input to obtain
     - Multi-scale FFT information
@@ -71,14 +71,18 @@ class AudioFeaturesDataset(Dataset):
         self.extractors = {}
         self.extractors['f0'] = FundamentalFrequency(args.sr, args.block_size, args.sequence_size).float()
         self.extractors['loudness'] = Loudness(args.block_size, args.kernel_size).float()
-    
+
     def preprocess_dataset(self, datadir):
         cur_id = 0
         for cur_file in self.data_files:
             # Keep the current file name
             f_name, _ = os.path.splitext(os.path.basename(cur_file))
             # Import audio
-            y, sr = librosa.core.load(cur_file) 
+            y, sr = librosa.core.load(cur_file)
+            # Resample to args.sr
+            if sr != self.args.sr:
+                print("Resampling {} from {} to {}".format(f_name, sr, self.args.sr))
+                y = librosa.core.resample(y, sr, self.args.sr)
             # Compute all sequences
             mod = self.args.block_size * self.args.sequence_size
             # Reshape into batch x seq
@@ -91,13 +95,15 @@ class AudioFeaturesDataset(Dataset):
                 features['name'] = f_name
                 features['audio'] = y[i]
                 features['fft'] = cur_fft[i]
+                for zz in cur_fft[i]:
+                    assert zz.size(-1) != 3997, "fft size is {}".format(zz.size(-1))
                 # Compute features in dataset
                 for k, v in self.extractors.items():
                     features[k] = v(y[i])[:self.args.sequence_size]
                 # Save to numpy compressed format
                 np.save(datadir + '/data/seq_' + str(cur_id) + '.npy', features)
-                cur_id += 1    
-    
+                cur_id += 1
+
     def switch_set(self, name):
         if (name == 'test'):
             self.features_files = self.test_files[0]
@@ -125,32 +131,32 @@ class AudioFeaturesDataset(Dataset):
             self.var = self.var + ((data - self.mean) * b_var).mean()
         self.mean = float(self.mean)
         self.var = float(np.sqrt(self.var / len(self.features_files)))
-    
+
     def analyze_dataset(self):
         # Fill some properties based on the first file
         loaded = np.load(self.features_files[0], allow_pickle=True).item()
         # Here we simply get the input and output shapes
         self.input_size = loaded['audio'].shape
         self.output_size = self.input_size
-            
+
     def create_splits(self, splits, shuffle_files):
-            nb_files = len(self.features_files)
-            if (shuffle_files):
-                idx = np.random.permutation(nb_files).astype('int')
-                self.features_files = [self.features_files[i] for i in idx]
-            idx = np.linspace(0, nb_files-1, nb_files).astype('int')
-            train_idx = idx[:int(splits[0]*nb_files)]
-            valid_idx = idx[int(splits[0]*nb_files):int((splits[0]+splits[1])*nb_files)]
-            test_idx = idx[int((splits[0]+splits[1])*nb_files):]
-            # Validation split
-            self.valid_files = (
-                    [self.features_files[i] for i in valid_idx],
-                    None)
-            # Test split
-            self.test_files = (
-                    [self.features_files[i] for i in test_idx],
-                    None)
-            self.features_files = [self.features_files[i] for i in train_idx]
+        nb_files = len(self.features_files)
+        if (shuffle_files):
+            idx = np.random.permutation(nb_files).astype('int')
+            self.features_files = [self.features_files[i] for i in idx]
+        idx = np.linspace(0, nb_files-1, nb_files).astype('int')
+        train_idx = idx[:int(splits[0]*nb_files)]
+        valid_idx = idx[int(splits[0]*nb_files):int((splits[0]+splits[1])*nb_files)]
+        test_idx = idx[int((splits[0]+splits[1])*nb_files):]
+        # Validation split
+        self.valid_files = (
+                [self.features_files[i] for i in valid_idx],
+                None)
+        # Test split
+        self.test_files = (
+                [self.features_files[i] for i in test_idx],
+                None)
+        self.features_files = [self.features_files[i] for i in train_idx]
 
     def __getitem__(self, idx):
         loaded = np.load(self.features_files[idx], allow_pickle=True).item()

code/ddsp/analysis.py
@@ -102,7 +102,9 @@ class MultiscaleFFT(Analysis):
         stfts = []
         for i, scale in enumerate(self.scales):
             cur_fft = torch.stft(x, n_fft=scale, window=self.windows[i], hop_length=int((1-self.overlap)*scale), center=False)
-            stfts.append(amp(cur_fft))
+            amplitude = amp(cur_fft)
+            # assert amplitude.size(-1) != 1997, "scale {}, x_len {}, hop_len {}".format(scale, x.size(), int((1-self.overlap)*scale))
+            stfts.append(amplitude)
         if (self.reshape):
             stft_tab = []
             for b in range(x.shape[0]):

code/ddsp/loss.py
@@ -23,7 +23,7 @@ amp = lambda x: x[...,0]**2 + x[...,1]**2
 class MSSTFTLoss(Loss):
     """
     Compute the FFT of a signal at multiple scales
-    
+
     Arguments:
             block_size (int)    : size of a block of conditionning
             sequence_size (int) : size of the conditioning sequence
@@ -44,8 +44,10 @@ class MSSTFTLoss(Loss):
         # First compute multiple STFT for x
         for i, scale in enumerate(self.scales):
             cur_fft = torch.stft(x, n_fft=scale, window=self.windows[i], hop_length=int((1-self.overlap)*scale), center=False)
-            stfts.append(amp(cur_fft))
-        # Compute loss 
-        lin_loss = sum([torch.mean(abs(stfts_orig[i][j] - stfts[i][j])) for j in range(len(stfts[i])) for i in range(len(stfts))])
-        log_loss = sum([torch.mean(abs(torch.log(stfts_orig[i][j] + 1e-4) - torch.log(stfts[i][j] + 1e-4)))  for j in range(len(stfts[i])) for i in range(len(stfts))])
+            amplitude = amp(cur_fft)
+            # assert amplitude.size(-1) != 3997, "scale {}, x_len {}, hop_len {}".format(scale, x.size(), int((1-self.overlap)*scale))
+            stfts.append(amplitude)
+        # Compute loss
+        lin_loss = sum([torch.mean(abs(stfts_orig[i][j].to(amplitude.device) - stfts[i][j].to(amplitude.device))) for j in range(len(stfts[i])) for i in range(len(stfts))])
+        log_loss = sum([torch.mean(abs(torch.log(stfts_orig[i][j].to(amplitude.device) + 1e-4) - torch.log(stfts[i][j].to(amplitude.device) + 1e-4))) for j in range(len(stfts[i])) for i in range(len(stfts))])
         return lin_loss + log_loss

code/ddsp/oscillators.py
@@ -23,7 +23,7 @@ class HarmonicOscillators(Oscillator):
         super(Oscillator, self).__init__()
         self.apply(self.init_parameters)
         self.n_partial = n_partial
-        self.upsample = nn.Upsample(scale_factor = block_size, mode="linear", align_corners=False)
+        self.upsample = nn.Upsample(scale_factor=block_size, mode="linear", align_corners=False)
         self.k = nn.Parameter(torch.arange(1, n_partial + 1).reshape(1,1,-1).float(), requires_grad=False)
         self.sample_rate = sample_rate
     
@@ -45,7 +45,7 @@ class HarmonicOscillators(Oscillator):
         # Generate the output signal
         y =  amp * torch.sum(anti_alias * alpha * torch.sin(self.k * phi), -1)
         return y
-    
+
     def set_parameters(self, params, batch_dim=64):
         """ Set parameters values (sub-modules) """
         self.amp = params[:, :, 0].unsqueeze(2)

code/model.py
@@ -39,7 +39,7 @@ class DDSSynth(AE):
         self.synth = synth
         if (upsampler is None):
             self.upsampler = nn.Upsample(scale_factor=args.block_size, mode="linear", align_corners=False)
-    
+
     def decode(self, z):
         """
         Decoding function of the DDSSynth.
@@ -64,7 +64,7 @@ class DDSSynth(AE):
         # Decode the samples
         x_tilde = self.decode((z_tilde, condition))
         return x_tilde, z_tilde, z_loss
-    
+
     def train_epoch(self, loader, loss, optimizer, args):
         self.train()
         full_loss = 0
@@ -85,7 +85,7 @@ class DDSSynth(AE):
             full_loss += b_loss
         full_loss /= len(loader)
         return full_loss
-    
+
     def eval_epoch(self, loader, loss, args):
         self.eval()
         full_loss = 0
@@ -129,7 +129,7 @@ class DDSSynthWAE(DDSSynth, WAE):
     def __init__(self, encoder, decoder, synth, args, upsampler=None):
         DDSSynth.__init__(self, encoder, decoder, synth, args, upsampler)
         WAE.__init__(self)
-        
+
 """
 ###################
 

code/utils/plot.py
@@ -22,9 +22,9 @@ def plot_batch_detailed(batch, n_examples = 4, name=None):
     # Create one big image for plot
     fig, axes = plt.subplots(n_examples, 9, figsize=(20, 10))
     for b in range(min(n_examples, audio.shape[0])):
-        axes[b, 0].plot(audio[b].squeeze(0)[:2000])
-        axes[b, 1].plot(f0[b].squeeze(0), 'r')
-        axes[b, 2].plot(loudness[b].squeeze(0), 'g')
+        axes[b, 0].plot(audio[b].cpu().squeeze(0)[:2000])
+        axes[b, 1].plot(f0[b].cpu().squeeze(0), 'r')
+        axes[b, 2].plot(loudness[b].cpu().squeeze(0), 'g')
         for w in range(6):
             axes[b, w+3].plot(np.log(fft[w][b, :, 0] + 1e-3))#, aspect='auto')
     if (name is not None):