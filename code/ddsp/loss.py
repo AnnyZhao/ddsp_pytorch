56e82aa21fa2a69fbefa307f7d1f5c14e53d745d
Anny Zhao <anzhao0505@gmail.com>
2020-02-01 19:31:08 -0600

    edit loss function

--- code/ddsp/loss.py
+++ code/ddsp/loss.py
@@ -4,13 +4,13 @@ import torch
 import torch.nn as nn
 import numpy as np
 from torch.nn.modules.loss import _Loss
-    
+
 class Loss(_Loss):
-    
+
     def __init__(self):
         super(Loss, self).__init__()
         self.apply(self.init_parameters)
-    
+
     def init_parameters(self, m):
         pass
 
@@ -20,34 +20,48 @@ class Loss(_Loss):
 # Lambda for computing squared amplitude
 amp = lambda x: x[...,0]**2 + x[...,1]**2
 
-class MSSTFTLoss(Loss):
-    """
-    Compute the FFT of a signal at multiple scales
 
-    Arguments:
-            block_size (int)    : size of a block of conditionning
-            sequence_size (int) : size of the conditioning sequence
-    """
-    
-    def __init__(self, scales, overlap=0.75):
-        super(MSSTFTLoss, self).__init__()
-        self.apply(self.init_parameters)
+class MSSTFT(nn.Module):
+    def __init__(self, scales=[64, 128, 256, 512], overlap=0.75):
+        super(MSSTFT, self).__init__()
         self.scales = scales
         self.overlap = overlap
-        self.windows = nn.ParameterList(nn.Parameter(torch.from_numpy(np.hanning(scale)).float(), requires_grad=False) for scale in self.scales)
-    
+        self.windows = nn.ParameterList(
+            nn.Parameter(torch.from_numpy(np.hanning(scale)).float(), requires_grad=False)
+            for scale in self.scales)
+
+    def forward(self, x):
+        stfts = []
+        device = x.device
+        # Compute multiple STFT for x
+        for i, scale in enumerate(self.scales):
+            cur_fft = torch.stft(x, n_fft=scale, window=self.windows[i], hop_length=int((1 - self.overlap) * scale), center=False)
+            stfts.append(amp(cur_fft).to(device))
+        return stfts
+
+
+class MSSTFTLoss(Loss):
+    """
+    Multi-scale STFT loss.
+    Compute the FFT of a signal at multiple scales.
+
+    Arguments:
+            scales ([int])  : array of N_FFT paramters, corresponding to the different scales of FFT.
+            overlap (float) : amount of overlap of the FFT windows.
+    """
+
+    def __init__(self, scales=[64, 128, 256, 512], overlap=0.75):
+        super(MSSTFTLoss, self).__init__()
+        self.apply(self.init_parameters)
+        self.msstft = MSSTFT(scales=scales, overlap=overlap)
+
     def init_parameters(self, m):
         pass
 
     def forward(self, x, stfts_orig):
-        stfts = []
         # First compute multiple STFT for x
-        for i, scale in enumerate(self.scales):
-            cur_fft = torch.stft(x, n_fft=scale, window=self.windows[i], hop_length=int((1-self.overlap)*scale), center=False)
-            amplitude = amp(cur_fft)
-            # assert amplitude.size(-1) != 3997, "scale {}, x_len {}, hop_len {}".format(scale, x.size(), int((1-self.overlap)*scale))
-            stfts.append(amplitude)
+        stfts = self.msstft(x)
         # Compute loss
-        lin_loss = sum([torch.mean(abs(stfts_orig[i][j].to(amplitude.device) - stfts[i][j].to(amplitude.device))) for j in range(len(stfts[i])) for i in range(len(stfts))])
-        log_loss = sum([torch.mean(abs(torch.log(stfts_orig[i][j].to(amplitude.device) + 1e-4) - torch.log(stfts[i][j].to(amplitude.device) + 1e-4))) for j in range(len(stfts[i])) for i in range(len(stfts))])
+        lin_loss = sum([torch.mean(torch.abs(stfts_orig[i][j] - stfts[i][j])) for i in range(len(stfts)) for j in range(len(stfts[i])) ])
+        log_loss = sum([torch.mean(torch.abs(torch.log(stfts_orig[i][j] + 1e-4) - torch.log(stfts[i][j] + 1e-4))) for i in range(len(stfts)) for j in range(len(stfts[i])) ])
         return lin_loss + log_loss
